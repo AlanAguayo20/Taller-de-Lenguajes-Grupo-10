7df7e9fc8fd2e974e7ad1fb5e7ef06b4bbec4b23
fedeeecuu <fede_j_quiroga@hotmail.com>
2026-07-08 13:05:53 -0300

    Update main.js
    Signed-off-by: fedeeecuu <fede_j_quiroga@hotmail.com>

--- js/main.js
+++ js/main.js
@@ -21,3 +21,47 @@ boton.addEventListener("click", () => {
         localStorage.setItem("tema", "oscuro");
     }
 });
+
+//formulario
+
+const formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    let nombre = document.getElementById("nombre").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let juego = document.getElementById("juego").value;
+
+    let respuesta = document.getElementById("respuesta");
+
+
+    if(nombre.length < 3){
+        respuesta.textContent = "El nombre debe tener al menos 3 caracteres.";
+        respuesta.style.color = "red";
+        return;
+    }
+
+
+    if(!email.includes("@")){
+        respuesta.textContent = "Ingresá un correo válido.";
+        respuesta.style.color = "red";
+        return;
+    }
+
+
+    if(juego === ""){
+        respuesta.textContent = "Seleccioná un juego.";
+        respuesta.style.color = "red";
+        return;
+    }
+
+
+    respuesta.textContent = "✅ ¡Inscripción enviada correctamente!";
+    respuesta.style.color = "green";
+
+    formulario.reset();
+
+});
+
